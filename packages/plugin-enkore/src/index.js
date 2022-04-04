58981305bc826928e757fb2311660407407c49f6
Steph Tyszka <steph@bootsa.net>
2022-04-04 15:43:16 +0200

    try with just input format for the moment

--- packages/plugin-enkore/src/index.js
+++ packages/plugin-enkore/src/index.js
@@ -14,7 +14,7 @@
 import { plugins } from "@citation-js/core"
 
 import { ref, formats as input } from "./input/index.js"
-import config from "./config.js"
-import output from "./output/index.js"
+// import config from "./config.js"
+// import output from "./output/index.js"
 
-plugins.add(ref, { input, output, config })
+plugins.add(ref, { input }) // { input, output, config })
